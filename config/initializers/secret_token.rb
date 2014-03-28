921761f901f86d957f5060472b2252736f4c263c
cjacks0413 <chjackson93@gmail.com>
2014-03-28 18:19:37 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SmithMusic::Application.config.secret_key_base = '966be4105b18ad90dd0edbf68071be52516053f8f22c594e8394e78a10b18eb033ad673b7db6fa6c6f4ae72c1b1d433970fdc3c8198deb860ecf2900b124db76'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+SmithMusic::Application.config.session_store :cookie_store, key: '_smith_music_session'
