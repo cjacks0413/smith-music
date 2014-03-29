0bf5090952fe269598c43a0a5c0df2c79767b7fc
cjacks0413 <chjackson93@gmail.com>
2014-03-28 21:35:49 -0500

    db update

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140329003603) do
+ActiveRecord::Schema.define(version: 20140328232236) do
 
   create_table "events", force: true do |t|
     t.string   "title"
@@ -30,8 +30,6 @@ ActiveRecord::Schema.define(version: 20140329003603) do
     t.string   "role"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "user_name"
-    t.string   "password_digest"
   end
 
 end
